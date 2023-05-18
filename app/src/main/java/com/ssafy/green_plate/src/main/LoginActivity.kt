179b2fc4a2722dc74743a255707000dbd384ca0b
jinyoung <kimjinzer099@gmail.com>
2023-05-18 17:24:32 +0900

    Feat : BottomNavigationBar 추가, Navigation 추가

--- app/src/main/java/com/ssafy/green_plate/src/main/LoginActivity.kt
+++ app/src/main/java/com/ssafy/green_plate/src/main/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.ssafy.green_plate
+package com.ssafy.green_plate.src.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ssafy.green_plate.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
